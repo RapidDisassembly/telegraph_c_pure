16e63b06a1a02172d6865a33f79a7cc5d3094073
RapidDisassembly <RapidDisassemblyGithub@protonmail.com>
2022-11-26 20:50:27 +0000

    remove code that refers to morse_to_bin_test

--- telegraph/main.c
+++ telegraph/main.c
@@ -4,5 +4,5 @@
 
 int main()
 {
-    morse_to_bin_test();
+
 }
--- telegraph/morse_bin.h
+++ telegraph/morse_bin.h
@@ -5,9 +5,9 @@
 //void single_morse_to_bin_test();
 
 int morse_to_bin(char * morse_str, int morse_str_size, char * bin_str, int bin_str_size);
-void morse_to_bin_test();
+//void morse_to_bin_test();
 
-//int bin_to_morse(char * bin_str, int bin_str_size, char * morse_str, int morse_str_size);
-//void bin_to_morse_test();
+int bin_to_morse(char * bin_str, int bin_str_size, char * morse_str, int morse_str_size);
+void bin_to_morse_test();
 
 #endif // MORSE_BIN_H_INCLUDED
